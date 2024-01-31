96ef621e36879b218da97c264e10fa44cb0bcc6d
Abror <155730665+AbrorXusanjonov@users.noreply.github.com>
2024-01-31 20:43:28 +0500

    Delete gitpod 24.01.2024 directory

--- gitpod 24.01.2024/app/app.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector('.burger');
-let screen = document.querySelector('.screen');
-function getActive() {
-    burger.classList.toggle('_active');
-    screen.classList.toggle('_active');
-}
